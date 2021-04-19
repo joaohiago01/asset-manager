6f907c54930aed09a8692ba339140dd37b90ce68
joaohiago01 <joaohiago96@gmail.com>
2021-04-18 23:17:48 -0300

    Adiciona configurações em variável de ambiente, muda script start para usar SSL, altera favicon e adiciona estilo global

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  baseApiURL: 'https://localhost:8081',
+  usernameApi: 'asset-manager-app',
+  passwordApi: 'manager-web-system-ifpb'
 };
