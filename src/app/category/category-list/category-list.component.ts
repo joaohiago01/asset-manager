8dcbc70592a8ae6b26711cf7ec9ae08905f9f1b5
Pedro Carvalho <peag.carvalho@gmail.com>
2021-04-25 22:39:24 -0300

    Adicionar modal para cadastro de categoria

--- src/app/category/category-list/category-list.component.ts
+++ src/app/category/category-list/category-list.component.ts
@@ -8,16 +8,17 @@ import { CategoryService } from '../services/category.service';
 @Component({
   selector: 'app-category-list',
   templateUrl: './category-list.component.html',
-  styleUrls: ['./category-list.component.css']
+  styleUrls: ['./category-list.component.css'],
 })
 export class CategoryListComponent implements OnInit {
   public categories: Category[] = [];
+  categoryType = CategoryType;
 
   constructor(
     public httpClient: HttpClient,
     public router: Router,
     public categoryService: CategoryService
-  ) { }
+  ) {}
 
   async ngOnInit(): Promise<void> {
     await this.getAllCategories();
@@ -26,45 +27,59 @@ export class CategoryListComponent implements OnInit {
   async getAllCategories(): Promise<void> {
     this.categories = await this.categoryService.getAllCategories();
     if (!this.categories) {
-      alert("Nenhuma Categoria Encontrada");
+      alert('Nenhuma Categoria Encontrada');
     }
   }
 
-  async createCategory(name: string, categoryType: CategoryType): Promise<void> {
+  async createCategory(
+    name: string,
+    categoryType: CategoryType
+  ): Promise<void> {
     if (name && categoryType) {
       let category = new Category({
         name,
-        categoryType
+        categoryType,
       });
-      let categoryWasCreated = await this.categoryService.createCategory(category);
+      let categoryWasCreated = await this.categoryService.createCategory(
+        category
+      );
       if (categoryWasCreated === true) {
         window.location.reload();
       } else {
-        alert("Oops, ocorreu um erro ao tentar cadastrar essa Categoria");
+        alert('Oops, ocorreu um erro ao tentar cadastrar essa Categoria');
       }
     } else {
-      alert("Dados Inválidos");
+      alert('Dados Inválidos');
     }
   }
 
   detailCategory(categoryId: number) {
-    return this.categories.find((category: Category) => category.id === categoryId);
+    return this.categories.find(
+      (category: Category) => category.id === categoryId
+    );
   }
 
-  async editCategory(id: number, name: string, categoryType: CategoryType): Promise<void> {
+  async editCategory(
+    id: number,
+    name: string,
+    categoryType: CategoryType
+  ): Promise<void> {
     if (name && categoryType) {
-      let category = new Category({
-        name,
-        categoryType
-      }, id);
+      let category = new Category(
+        {
+          name,
+          categoryType,
+        },
+        id
+      );
       let categoryWasEdited = await this.categoryService.editCategory(category);
       if (categoryWasEdited === true) {
         window.location.reload();
       } else {
-        alert("Oops, ocorreu um erro ao tentar editar essa Categoria");
+        alert('Oops, ocorreu um erro ao tentar editar essa Categoria');
       }
     } else {
-      alert("Dados Inválidos");
+      alert('Dados Inválidos');
     }
   }
 
@@ -74,11 +89,37 @@ export class CategoryListComponent implements OnInit {
       if (categoryWasDeleted === true) {
         window.location.reload();
       } else {
-        alert("Oops, ocorreu um erro ao tentar remover essa Categoria");
+        alert('Oops, ocorreu um erro ao tentar remover essa Categoria');
       }
     } else {
-      alert("Categoria Inválida");
+      alert('Categoria Inválida');
     }
   }
-
 }
+
+const showModal = function () {
+  const modal: HTMLDivElement = <HTMLDivElement>(
+    document.querySelector('.modal-window')
+  );
+  const overlay: HTMLDivElement = <HTMLDivElement>(
+    document.querySelector('.overlay')
+  );
+
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const hideModal = function () {
+  const modal: HTMLDivElement = <HTMLDivElement>(
+    document.querySelector('.modal-window')
+  );
+  const overlay: HTMLDivElement = <HTMLDivElement>(
+    document.querySelector('.overlay')
+  );
+
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+document.querySelector('btnCadastrar')?.addEventListener('click', showModal);
+document.querySelector('btnFechar')?.addEventListener('click', hideModal);
