89fa37213808d8fb182af0f788dc536daabd4561
Maycon <may.karvalhooo@gmail.com>
2021-04-29 20:20:54 -0300

    Componente de cadastro de equipamento

--- src/app/equipments/equipments.module.ts
+++ src/app/equipments/equipments.module.ts
@@ -7,6 +7,8 @@ import { SharedModule } from '../shared/shared.module';
 import { MatButtonModule } from '@angular/material/button';
 import { EquipmentCreateComponent } from './equipment-create/equipment-create.component';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFileUploadModule } from 'angular-material-fileupload';
 
 @NgModule({
   declarations: [
@@ -18,7 +20,9 @@ import { MatFormFieldModule } from '@angular/material/form-field';
     CommonModule,
     EquipmentsRoutingModule,
     MatButtonModule,
-    MatFormFieldModule
+    MatFormFieldModule,
+    MatSelectModule,
+    MatFileUploadModule
   ]
 })
 export class EquipmentsModule { }
