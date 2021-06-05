a78dac94b7b9f5c18dc21fb8bd5a0ecaea184408
João Hiago <jsantos@aiko.digital>
2021-06-05 12:02:55 -0300

    Cria layout para cadastro e edição de Insumo

--- src/app/assets/assets.module.ts
+++ src/app/assets/assets.module.ts
@@ -3,12 +3,22 @@ import { CommonModule } from '@angular/common';
 
 import { AssetsRoutingModule } from './assets-routing.module';
 import { AssetFormComponent } from './asset-form/asset-form.component';
+import { SharedModule } from '../shared/shared.module';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [AssetFormComponent],
   imports: [
+    SharedModule,
     CommonModule,
-    AssetsRoutingModule
+    AssetsRoutingModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatSelectModule,
+    MatButtonModule
   ]
 })
 export class AssetsModule { }
