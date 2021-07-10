f8950fe70491bea187eef84ff8146329acfa4b7c
Pedro Carvalho <peag.carvalho@gmail.com>
2021-07-09 22:45:16 -0300

    feat: Adiciona os testes de Setores

--- src/app/departments/departments-list/departments-list.component.spec.ts
+++ src/app/departments/departments-list/departments-list.component.spec.ts
@@ -1,25 +1,51 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+// import { browser, element, by } from 'protractor';
 
-import { DepartmentsListComponent } from './departments-list.component';
+describe('Create or Edit New Department', () => {
+  beforeAll(() => {
+    browser.driver.manage().window().maximize();
+    browser.get('https://localhost:4200/login');
 
-describe('DepartmentsListComponent', () => {
-  let component: DepartmentsListComponent;
-  let fixture: ComponentFixture<DepartmentsListComponent>;
+    element(by.id('username')).getWebElement().sendKeys('admin');
+    browser.sleep(1000);
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ DepartmentsListComponent ]
-    })
-    .compileComponents();
+    element(by.id('password')).getWebElement().sendKeys('admin');
+    browser.sleep(1000);
+
+    element(by.id('login')).getWebElement().click();
+    browser.sleep(1000);
+
+    browser.get('https://localhost:4200/departments');
   });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(DepartmentsListComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  it('should create new department', () => {
+    element(by.id('addButton')).getWebElement().click();
+    browser.sleep(1000);
+
+    element(by.id('nameModalNew')).sendKeys('Coordenação de Análise e Desenvolvimento de Sistemas');
+    browser.sleep(1000);
+
+    element(by.id('acronymModalNew')).sendKeys('CCADS');
+    browser.sleep(1000);
+
+    element(by.id('create')).getWebElement().click();
+    browser.sleep(5000);
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should create new department with the same acronym', () => {
+    element(by.id('addButton')).getWebElement().click();
+    browser.sleep(1000);
+
+    element(by.id('nameModalNew')).sendKeys('Coordenação de ADS');
+    browser.sleep(1000);
+
+    element(by.id('acronymModalNew')).sendKeys('CCADS');
+    browser.sleep(1000);
+
+    element(by.id('create')).getWebElement().click();
+    browser.sleep(3000);
+
+    let alert = browser.switchTo().alert();
+    expect(alert.getText()).toEqual('Oops, ocorreu um erro ao tentar cadastrar esse setor');
   });
+  
 });
