595ed5703f558a38b99934d5d011f7476414755f
Pedro Carvalho <peag.carvalho@gmail.com>
2021-07-10 21:53:18 -0300

    Cria o serviço da Entrada de Insumos com os métodos get, post e delete

---
+++ src/app/assets/services/inputAsset.service.ts
@@ -0,0 +1,81 @@
+import { Injectable } from '@angular/core';
+import { Asset } from 'src/app/shared/models/asset.model';
+import { AuthenticationService } from 'src/app/shared/services/authentication.service';
+import api from 'src/app/shared/services/api';
+import { InputAsset } from 'src/app/shared/models/inputAsset.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class InputAssetService {
+  constructor(private authenticationService: AuthenticationService) {}
+
+  async saveInputAsset(assetId: number, inputAsset: InputAsset): Promise<boolean> {
+    let inputAssetWasAdded = false;
+    try {
+      let headers = {
+        Authorization: `Bearer ${this.authenticationService.token}`,
+      };
+
+      let inputAssetServer = {
+        data: new Date(),
+        dataValidade: new Date(),
+        quantidade: inputAsset.amount,
+      };
+      await api.post(`/insumos/${assetId}/entradas`, inputAssetServer, { headers });
+      inputAssetWasAdded = true;
+      return inputAssetWasAdded;
+    } catch (error) {
+      console.error(error);
+      return (inputAssetWasAdded = false);
+    }
+  }
+
+  async deleteInputAsset(assetId: number, inputAssetId: number): Promise<boolean> {
+    let inputAssetWasDeleted = false;
+    try {
+      let headers = {
+        Authorization: `Bearer ${this.authenticationService.token}`,
+      };
+
+      await api.delete(`/insumos/${assetId}/entradas/${inputAssetId}`, { headers });
+      inputAssetWasDeleted = true;
+
+      alert('Deletado com sucesso!');
+
+      return inputAssetWasDeleted;
+    } catch (error) {
+      console.error(error);
+      return (inputAssetWasDeleted = false);
+    }
+  }
+
+  async getAllInputs(assetId: number): Promise<InputAsset[]> {
+    try {
+      let inputs!: InputAsset[];
+
+      let headers = {
+        Authorization: `Bearer ${this.authenticationService.token}`,
+      };
+
+      const response = await api.get(`/insumos/${assetId}/entradas`, { headers });
+      inputs = response.data.map((inputAssetServer: any) => {
+        return new InputAsset(
+          {
+            inputDate: inputAssetServer.data,
+            expirationDate: inputAssetServer.dataValidade,
+            amount: inputAssetServer.quantidade,
+            assetId: inputAssetServer.insumo.id,
+            assetName: inputAssetServer.insumo.nome,
+          },
+          inputAssetServer.id
+        );
+      });
+
+      return inputs;
+    } catch (error) {
+      console.error(error);
+      return [];
+    }
+  }
+}
---
+++ src/app/shared/models/inputAsset.model.ts
@@ -0,0 +1,18 @@
+export class InputAsset {
+  public id!: number;
+  public inputDate: Date;
+  public expirationDate: Date;
+  public amount: number;
+  public assetId: number;
+  public assetName?: string;
+  
+  constructor(props: Omit<InputAsset, 'id'>, id?: number) {
+    this.inputDate = props.inputDate;
+    this.expirationDate = props.expirationDate;
+    this.amount = props.amount;
+    this.assetId = props.assetId;
+    if (id != undefined) {
+      this.id = id;
+    }
+  }
+}
